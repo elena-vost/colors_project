204d1e317741459e07e0b9f419cc2b2852d75f2a
elena-vost <angara1298@gmail.com>
2021-03-09 20:03:39 +0100

    classica

--- src/Palette.js
+++ src/Palette.js
@@ -2,6 +2,12 @@ import React, { Component } from "react";
 
 export default class Palette extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <h1>PALETTES</h1>
+      </div>
+    );
   }
 }
+
+export default Palette;
