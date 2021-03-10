87d24e98381a6454847d2e60cd83abf32d85a836
elena-vost <angara1298@gmail.com>
2021-03-10 20:26:48 +0100

    classica

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -5,15 +5,17 @@ class ColorBox extends Component {
   render() {
     const { name, background } = this.props;
     return (
-      <div style={{ background }} className="ColorBox">
-        <div className="copy-container">
-          <div className="box-content">
-            <span>{name}</span>
+      <CopyToClipboard text={background}>
+        <div style={{ background }} className="ColorBox">
+          <div className="copy-container">
+            <div className="box-content">
+              <span>{name}</span>
+            </div>
+            <button className="copy-button">Copy</button>
           </div>
-          <button className="copy-button">Copy</button>
+          <span className="see-more">More</span>
         </div>
-        <span className="see-more">More</span>
-      </div>
+      </CopyToClipboard>
     );
   }
 }
