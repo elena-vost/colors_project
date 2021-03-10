68c7469d20918956dbdf37634ec08b58aebe4746
elena-vost <angara1298@gmail.com>
2021-03-10 20:21:40 +0100

    classica

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./ColorBox.css";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 class ColorBox extends Component {
   render() {
     const { name, background } = this.props;
