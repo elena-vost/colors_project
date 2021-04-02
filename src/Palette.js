0d990ad91c27bed6966adfa6518a1879cb4deb10
elena-vost <angara1298@gmail.com>
2021-04-02 17:02:00 +0200

    classa

--- src/Palette.js
+++ src/Palette.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import ColorBox from "./ColorBox";
 import "./Palette.css";
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
 
 export class Palette extends Component {
   render() {
-    const colorBoxes = this.props.palette.colors[300].map((color) => (
-      <ColorBox background={color.color} name={color.name} />
+    const colorBoxes = this.props.palette.colors[500].map((color) => (
+      <ColorBox background={color.hex} name={color.name} />
     ));
     return (
       <div className="Palette">
