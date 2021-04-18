7b909e7b1cd3ee54a9360209a22e75ebba42af66
elena-vost <angara1298@gmail.com>
2021-04-18 16:47:11 +0200

    class11111a1

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
 import Snackbar from "@material-ui/core/Snackbar";
+import IconButton from "@material-ui/core/IconButton";
 import CloseIcon from "@material-ui/icons/Close";
 
 import Slider from "rc-slider";
@@ -53,6 +54,7 @@ class Navbar extends Component {
           ContentProps={{
             "aria-describedby": "message-id",
           }}
+          action={[<IconButton></IconButton>]}
         ></Snackbar>
       </header>
     );
